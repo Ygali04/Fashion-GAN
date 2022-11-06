ce8e3d25ac9a0ee29826a149e6395341c36754b1
Yahvin Gali <115979938+Ygali04@users.noreply.github.com>
2022-11-06 04:01:57 -0800

    Create Training.py

---
+++ Models/Training.py
@@ -0,0 +1,6 @@
+(train_images, train_labels), (_, _) = tf.keras.datasets.fashion_mnist.load_data()
+show_dataset_examples(train_images)
+print (train_images.shape)
+
+train_images_reshaped = np.expand_dims(train_images, 3)
+train_images_scaled = (train_images_reshaped - 127.5)/127.5
