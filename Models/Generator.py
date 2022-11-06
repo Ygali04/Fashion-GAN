6ad0df3800997388034c54cabc407b0ba992c333
Yahvin Gali <115979938+Ygali04@users.noreply.github.com>
2022-11-06 04:19:26 -0800

    Create Generator.py

---
+++ Models/Generator.py
@@ -0,0 +1,36 @@
+import Training
+from Data import StartingDataset
+
+def make_generator_model():
+    model = tf.keras.Sequential()
+    model.add(layers.Dense(7*7*256, use_bias=False, input_shape=(noise_dim,)))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Reshape((7, 7, 256)))
+    assert model.output_shape == (None, 7, 7, 256) 
+
+    model.add(layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
+    assert model.output_shape == (None, 7, 7, 128)
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+    assert model.output_shape == (None, 14, 14, 64)
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
+    assert model.output_shape == (None, 28, 28, 1)
+
+    return model
+  
+generator = make_generator_model()
+
+noise = tf.random.normal([1, noise_dim])
+generated_image = generator(noise, training=False)
+
+scale_and_show(generated_image[0])
+plt.show()
+
+print(generator.summary())
